458b44b968eec99ad34ef87541ce774826a0e4c5
Saaeen-Grant-UWI <saaeengrant.uwi@gmail.com>
2023-12-26 20:10:16 -0500

    Basic folder structure and basic Application class

---
+++ app/core/Application.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Tiny\core;
+
+final class Application
+{
+    
+    public function run() : void {
+        echo '<h1>Hello, World!</h1>';
+    }
+}
+
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use Tiny\core\Application;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new Application();
+$app->run();
